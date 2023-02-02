9a3e8344da6db885580af7f09a89fdf6511dfe46
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2023-02-02 12:53:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  { name: 'Dr. Mahfuz', job: 'Singer' },
+  { name: 'Eva Rahman', job: 'Singer2' },
+  { name: 'Agun', job: 'sopno' },
+  { name: 'Shuvro', job: 'pathor' },
+];
+
 function App() {
+  const nayoks = [
+    'Rubel',
+    'BappaDa',
+    'Kuber',
+    'Jashim',
+    'Salman Shah',
+    'Riyaz',
+    'Razzak',
+    'Anwar',
+  ];
+  console.log(singers);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {nayoks.map((nayok) => (
+        <li>Name: {nayok}</li>
+      ))}
+      {/* {nayoks.map((nayok) => (
+        <Person name={nayok} />
+      ))} */}
+
+      {singers.map((singer) => (
+        <Person name={singer.name} />
+      ))}
+
+      {/* <Person name={nayoks[0]} nayika='moushumi' />
+      <Person name='BappaRaz' nayika='cheka' />
+      <Person name='Kuber' nayika='Kopila' />
+      <Person /> */}
+
+      <h5>New component....</h5>
+      <p>rock mama React mama.</p>
+      <Friend movie='Shingam' phone='01777777' />
+      <Friend phone='019999999999' />
+      <Friend />
+    </div>
+  );
+}
+function Person(props) {
+  //console.log(props);
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      {/* <p>Nayika: {props.nayika}</p> */}
+    </div>
+  );
+}
+
+function Friend(props) {
+  // console.log(props);
+  return (
+    <div className='container'>
+      <h3>Name: {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
     </div>
   );
 }
